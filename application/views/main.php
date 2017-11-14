93a2ccf3b4384128d8aecfedb207d9aaea4a9924
webdev <webdev271@gmail.com>
2017-11-14 18:12:53 +0300

    implementing main layout continues-menu was modified, font-awesome folder was added

--- application/views/main.php
+++ application/views/main.php
@@ -2,27 +2,31 @@
 <html lang="en">
     <head>
         <title>title</title>
-<!--        <link href="<?php echo base_url() ?>public/css/bootstrap.min.css" type="text/css" rel="stylesheet"/> 
-        <link href="<?php echo base_url() ?>public/css/custom.css" type="text/css" rel="stylesheet"/> -->
-          <link href="http://localhost:8080/wakukayaweb/public/css/bootstrap.min.css" type="text/css" rel="stylesheet"/> 
-          <link href="http://localhost:8080/wakukayaweb/public/open-iconic/font/css/open-iconic.css" type="text/css" rel="stylesheet"/> 
+        <link href="<?php echo base_url() ?>public/css/bootstrap.min.css" type="text/css" rel="stylesheet"/> 
+        <link href="<?php echo base_url() ?>public/css/custom.css" type="text/css" rel="stylesheet"/> 
+          <!--<link href="http://localhost:8080/wakukayaweb/public/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>--> 
+          <!--<link href="http://localhost:8080/wakukayaweb/public/open-iconic/font/css/open-iconic.css" type="text/css" rel="stylesheet"/>-->
+          <link href="<?php echo base_url() ?>public/font-awesome-4.7.0/css/font-awesome.min.css"
         <link href="http://localhost:8080/wakukayaweb/public/css/custom.css" type="text/css" rel="stylesheet"/> 
+        
 
     </head>
     <body>
-        <div class="col-md-12 main-top">
-            <div class="left-panel col-md-4">
-                <h2 style="color: #fff;">WAKUKAYA</h2>
-                <!--<span class="oi" data-glyph="spreadsheet" title="login" aria-hidden="true">jcabjabdjkajk</span>-->
+        <div class="row main-top">
+            <div class="col-sm-4 logo">
+                <p class="h3" style="color: #c3c7ca;">WAKUKAYA</p>
             </div>
-            <div class="right-panel col-md-8">
+            <div class="col-sm-8 user-panel-links">
                 <ul>
-                    <li>Icon </li>
-                    <li>Hello Hassan Lyawile |</li>
-                    <li><a href="">Settings</a> |</li>
-                    <li><a href="">Logout</a></li>
+                    <li class="applyW"><span class="fa fa-user"></span> </li>
+                    <li class="applyW">Hello Hassan Lyawile <span class="applyW">|</span></li>
+                    <li class="applyY"><a href="">Settings</a> <span class="applyW">|</span></li>
+                    <li class="applyY"><a href="">Logout</a></li>
                 </ul>
             </div>
+            
+        </div>
+        <div class="row">
             
         </div>
     </body>
